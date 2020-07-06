072419598da40f37ec623714864e2d8c581e643c
henrique-temponi <tanzenheavely@gmail.com>
2020-07-06 09:52:59 -0300

    Criando a pagina principal

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -13,10 +13,12 @@
         <nav>
             <div class="nav-wrapper blue">
                 <div class="container">
-                    <a href="#!" class="brand-logo">Logo</a>
+                    <a href="#!" class="brand-logo">Site Dinamico</a>
                     <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                     <ul class="right hide-on-med-and-down">
                         <li><a href="#">Home</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Contato</a></li>
                     </ul>
                 </div>
             </div>
@@ -24,6 +26,8 @@
 
         <ul class="sidenav" id="mobile-demo">
             <li><a href="#">Home</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Contato</a></li>
         </ul>
 
         @yield('content')
---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.site')
+
+@section('content')
+
+<div class="container">
+    <h1>Home</h1>
+</div>
+
+@endsection
