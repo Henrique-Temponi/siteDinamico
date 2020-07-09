160804477d404b947f220665e1922070bec77c88
henrique-temponi <tanzenheavely@gmail.com>
2020-07-09 14:41:48 -0300

    Adicona tabela imoveis

--- app/Cidade.php
+++ app/Cidade.php
@@ -10,4 +10,9 @@ class Cidade extends Model
     protected $fillable = [
         'nome', 'estado', 'sigla_estado',
     ];
+
+    public function imoveis()
+    {
+        return $this->hasMany('App\Imovel', 'cidade_id');
+    }
 }
--- app/Tipo.php
+++ app/Tipo.php
@@ -10,4 +10,9 @@ class Tipo extends Model
     protected $fillable = [
         'titulo',
     ];
+
+    public function imoveis(){
+
+        return $this->hasMany('App\Imovel', 'tipo_id');
+    }
 }
